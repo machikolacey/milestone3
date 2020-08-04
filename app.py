21767092ef09e8d60b62b5ba498f766985d7bae6
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-08-04 23:26:15 +0000

    Added 'Add Cafe' page

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'brightonCafes'
+app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster.1nsni.mongodb.net/brightonCafes?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_cafes')
+def get_cafes():
+    return render_template("cafes.html", cafes=mongo.db.cafes.find())
+
+
+@app.route('/add_cafe')
+def add_task():
+    return render_template('addcafe.html')
 
 
 if __name__ == '__main__':
