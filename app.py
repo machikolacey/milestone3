cc843b7d72b992e809277614edfb02573dff712a
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-08-04 23:54:54 +0000

    Added insert_cafe action

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ def get_cafes():
     return render_template("cafes.html", cafes=mongo.db.cafes.find())
 
 
+@app.route('/insert_cafe', methods=["POST"])
+def insert_cafe():
+    cafes = mongo.db.cafes
+    cafes.insert_one(request.form.to_dict())
+    return redirect(url_for("get_cafes"))
+
+
 @app.route('/add_cafe')
 def add_task():
     return render_template('addcafe.html')
