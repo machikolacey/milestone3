8537cdfff0dbf907d2f20bddf29be2da1ca62a08
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-08-06 21:16:23 +0000

    Added login Logout functionality

--- app.py
+++ app.py
@@ -75,11 +75,24 @@ def login():
             return redirect (url_for("login"))
     return render_template("login.html")           
 
+@app.route('/logout')
+def logout():
+    flash('You have been logged out')
+    session.pop("user")
+    return redirect(url_for("login"))
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    username = mongo.db.users.find_one(
+    
+
+      username = mongo.db.users.find_one(
         {"username" : session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+
+      if session["user"]:
+         return render_template("profile.html", username=username)
+    
+         return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
