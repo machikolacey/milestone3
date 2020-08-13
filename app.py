6109dfd478f6170784c044a1edef9598f635fa4c
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-08-13 21:43:51 +0000

    Added fileter

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId 
 from bson import json_util
+import json
 
 app = Flask(__name__)
 
