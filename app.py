e79399e7983985a957e8cf5c8a7d3eee58e69e06
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-09-19 19:14:07 +0000

    Debug to be false

--- app.py
+++ app.py
@@ -379,4 +379,4 @@ def cafe_autocomplete(request, **kwargs):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
