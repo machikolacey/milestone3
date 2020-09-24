3646e2988d133ac460f1d8e81f7cdf0b98ffbf1d
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-09-24 21:02:14 +0000

    Fixing Responsive issues

--- static/js/custom-nouser.js
+++ static/js/custom-nouser.js
@@ -5,13 +5,10 @@
             preventScrolling : false,
             complete: function() { 
               setCookie("popup", true, 240);
-                
-           
              } 
             }
         );
   
-        //if(checkCookie("popup") == false ){
-
-              $('#modal1').modal('open');
-        //}          
+    if(checkCookie("popup") == false ){
+    $('#modal1').modal('open');
+    }          
