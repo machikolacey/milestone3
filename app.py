ea3c61dd5743da1b0301773c6c91cb2dfe63f3c6
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-02-12 20:46:35 +0000

    Added error handlers (404, 500)

--- app.py
+++ app.py
@@ -364,6 +364,16 @@ def your_account(username):
         return redirect(url_for('login'))
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
